9ba4ab18ed9f1b29a2ac8047aec46b873acb111a
Mikkel Rask <mikkelrask@protonmail.com>
2020-10-02 15:17:03 +0200

    Trying again

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ from pick import pick
 import pywhatkit as kit
 
 title = "What hermit do you want to watch?"
-options = ['Mumbo Jumbo', 'Xisumavoid', 'Etho\'s Lab', 'Good Times With Scar', 'BDoubleO', 'Cubfan135', 'False', 'Grian', 'Hypno', 'Impulse', 'Iskall', 'Jevin', 'Joe Hills', 'Keralis', 'Rendog', 'Stressmonster', 'Tango Tek', 'TFC', 'Vintage Beef', 'Welsknight', 'xBCrafted', 'Zadaph', 'Zombie Cleo']
+options = ['Mumbo Jumbo', 'Xisumavoid', 'Etho\'s Lab', 'Good Times With Scar', 'BDoubleO', 'Cubfan135', 'FalseSymmetry', 'Grian', 'Hypnotizd', 'ImpulseSV', 'Iskall85', 'iJevin', 'joehills', 'Keralis', 'Rendog', 'Stressmonster101', 'Tango Tek', 'Tin Foil Chef', 'Vintage Beef', 'Welsknight', 'xBCrafted', 'Zadaph', 'ZombieCleo']
 option, index = pick(options, title)
 print('Good choice!\nOpening latest video with '+option+' in your browser')
 kit.playonyt(option)
